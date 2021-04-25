30ef6edd85920d6b67ec3ae825b24848fadc4d13
BCraine <brandoncraine193@gmail.com>
2021-04-25 16:50:45 -0400

    have main code done working on pytests

--- tests/tests_490hw.py
+++ tests/tests_490hw.py
@@ -5,4 +5,5 @@ def test_happy(monkeypatch):
     monkeypatch.setattr('builtins.input', lambda _: "Massachusetts")
     state = input("Please enter a state to do business in.\n"
                   "You may pick between Massachusetts, New Hampshire, or Maine\n")
+
     assert state == "Massachusetts"
